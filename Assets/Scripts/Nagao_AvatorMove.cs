52b5d4341b04649dd8ca4cba95f8ff4315cfb4dd
FT-THINKPADPGEN\nagao <ryota.nagao@forevertail.com>
2021-07-17 15:11:15 +0900

    nagao

--- Assets/Scripts/Nagao_AvatorMove.cs
+++ Assets/Scripts/Nagao_AvatorMove.cs
@@ -5,11 +5,13 @@ using Photon.Pun;
 
 public class Nagao_AvatorMove : MonoBehaviourPunCallbacks
 {
-    [SerializeField] private float moveSpeed = 15f;
-    // Start is called before the first frame update
-    void Start()
-    {
+    private MeshRenderer mesh;
+    public Material[] colors;
+    private float moveSpeed = 20f;
 
+    private void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -17,11 +19,40 @@ public class Nagao_AvatorMove : MonoBehaviourPunCallbacks
     {
         if (photonView.IsMine)
         {
-            Vector3 v3 = new Vector3(
-                Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed,
+            Vector3 v = new Vector3(
+                Input.GetAxis("Horizontal") * moveSpeed*Time.deltaTime,
                 0,
-                Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed);
-            transform.position += v3;
-        };
+                Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+
+            //ˆÚ“®‚·‚é
+            transform.position += v;
+
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                photonView.RPC(nameof(SetColor), RpcTarget.All, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+
+                photonView.RPC(nameof(SetColor), RpcTarget.All, 1);
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+
+                photonView.RPC(nameof(SetColor), RpcTarget.All, 2);
+
+            }
+        }
+
+    }
+
+    [PunRPC]
+    void SetColor(int colorNum)
+    {
+        mesh.material = colors[colorNum];
     }
 }
