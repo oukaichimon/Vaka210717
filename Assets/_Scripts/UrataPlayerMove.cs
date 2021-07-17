bf9518eb4e79aeff8a450f365a0a0d00a54c76af
masashi_urata <0pz3132178@gmail.com>
2021-07-17 15:19:47 +0900

    復元

--- Assets/_Scripts/UrataPlayerMove.cs
+++ Assets/_Scripts/UrataPlayerMove.cs
@@ -8,6 +8,14 @@ public class UrataPlayerMove : MonoBehaviourPunCallbacks
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotSpeed = 100f;
 
+    private MeshRenderer meshRenderer;
+    [SerializeField] float duration = 2f;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -19,9 +27,18 @@ public class UrataPlayerMove : MonoBehaviourPunCallbacks
                 Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
 
             transform.position += v3;
+            Quaternion rot = Quaternion.AngleAxis(Time.deltaTime * rotSpeed, Vector3.up);
+            transform.rotation = rot * transform.localRotation;
+
+            photonView.RPC(nameof(SetColor), RpcTarget.All);
         }
 
-        Quaternion rot = Quaternion.AngleAxis(Time.deltaTime * rotSpeed, Vector3.up);
-        transform.rotation = rot * transform.localRotation;
     }
-}
+
+    [PunRPC]
+    void SetColor()
+    {
+        float phi = Time.time / duration * 2 * Mathf.PI;
+        float amp = Mathf.Cos(phi) * .5f + .5f;
+        meshRenderer.material.color = Color.HSVToRGB(amp, 1, 1);
+    }
