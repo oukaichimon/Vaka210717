b65be7d786946cbc40c322cbc6e50c98bbabab6c
oukaichimon <cxn01355@gmail.com>
2021-07-17 14:19:19 +0900

    福田まてりあう

--- Assets/Scripts/Fukuda_AvatarMove.cs
+++ Assets/Scripts/Fukuda_AvatarMove.cs
@@ -5,6 +5,16 @@ using Photon.Pun;
 
 public class Fukuda_AvatarMove : MonoBehaviourPunCallbacks
 {
+
+    private MeshRenderer mesh;
+
+    public Material[] colors;
+
+    private void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -17,6 +27,28 @@ public class Fukuda_AvatarMove : MonoBehaviourPunCallbacks
 
             //ˆÚ“®‚·‚é
             transform.position += v;
+
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                SetColor(0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                SetColor(1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                SetColor(2);
+            }
         }
+
+    }
+
+    void SetColor(int colorNum)
+    {
+        mesh.material = colors[colorNum];
     }
 }
