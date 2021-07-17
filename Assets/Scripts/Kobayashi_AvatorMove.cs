fa3c06e017c39083f6e26a71adcb25eb5af39d6f
kobayashi0510 <knotyknohnem@gmail.com>
2021-07-17 15:15:54 +0900

    小林_修正

--- Assets/Scripts/Kobayashi_AvatorMove.cs
+++ Assets/Scripts/Kobayashi_AvatorMove.cs
@@ -6,19 +6,61 @@ using Photon.Realtime;
 
 public class Kobayashi_AvatorMove : MonoBehaviourPunCallbacks
 {
+    private MeshRenderer mesh;
+    public Material[] colors;
 
-    // Start is called before the first frame update
+    private void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
     void Update()
     {
         if (photonView.IsMine)
         {
-            Vector3 v = new Vector3(
-                Input.GetAxis("Horizontal") * Time.deltaTime,
-                0,
-                Input.GetAxis("Vertical") * Time.deltaTime);
-
-            //移動するこーど
-            transform.position += v;
+            MyAvatarUpdate();
         }
+
+
+        //ほかの管理コード
+    }
+
+    private void MyAvatarUpdate()
+    {
+        Vector3 v = new Vector3(
+    Input.GetAxis("Horizontal") * Time.deltaTime,
+    0,
+    Input.GetAxis("Vertical") * Time.deltaTime);
+
+        //移動する
+        transform.position += v;
+
+
+        //PhotonViewに対してメッセージ発信
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //nameofでメソッドの文字列を取得できる
+            //RpcTarget.Allで自分を含む全てのアバターにメッセージを投げる
+            photonView.RPC(nameof(SetColor), RpcTarget.All, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            photonView.RPC(nameof(SetColor), RpcTarget.All, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            photonView.RPC(nameof(SetColor), RpcTarget.All, 2);
+        }
+    }
+
+    //PhotonViewからメッセージ受け取り
+    //PunRPC属性をつけないと、メッセージを受け取れない
+    [PunRPC]
+    void SetColor(int colorNum)
+    {
+        mesh.material = colors[colorNum];
     }
 }
