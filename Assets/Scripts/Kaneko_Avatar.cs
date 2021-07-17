5fdd508e09c932bfa0e01c2c2b3bf57ad3ebb02d
fm17p004 <fm17p004@gmail.com>
2021-07-17 14:38:32 +0900

    kaneko追加

---
+++ Assets/Scripts/Kaneko_Avatar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Kaneko_Avatar : MonoBehaviourPunCallbacks
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            Vector3 v = new Vector3(
+                Input.GetAxis("Horizontal") * Time.deltaTime,
+                0,
+                Input.GetAxis("Vertical") * Time.deltaTime);
+
+
+            transform.position += v;
+        }
+    }
+}
